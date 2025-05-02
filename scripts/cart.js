dd477c2bbb831745086c7424353011fb1234ec91
fc60282 <fc60282@alunos.ciencias.ulisboa.pt>
2025-05-03 00:58:50 +0100

    Continued implementation

--- scripts/cart.js
+++ scripts/cart.js
@@ -104,7 +104,7 @@ window.addEventListener('userAuthenticated', (event) => {
                                     localStorage.removeItem("cartProducts");
                                     // Show success
                                     document.getElementById("paymentInterface").innerHTML = `
-                                    <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 60vh;">
+                                    <div class="d-flex flex-column align-items-center my-5 justify-content-center" style="min-height: 60vh;">
                                         <div class="text-success mb-4">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
                                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.97 10.03a.75.75 0 0 0 1.08-.02l3.992-4.99a.75.75 0 1 0-1.16-.96L7.477 8.417 5.383 6.323a.75.75 0 0 0-1.06 1.06l2.647 2.647z"/>
